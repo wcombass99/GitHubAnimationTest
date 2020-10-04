8880a6ee9b1a78b1673463d5c13927c954f67d0e
wcombass99 <70278459+wcombass99@users.noreply.github.com>
2020-10-04 19:26:13 -0400

    Animation commit

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+  {
+     if (Input.GetKeyDown(KeyCode.W))
+        {
+          musicSource.clip = musicClipOne;
+          musicSource.Play();
+          anim.SetInteger("State", 1);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+        {
+          musicSource.Stop();
+          anim.SetInteger("State", 0);
+
+        }
+
+     if (Input.GetKeyDown(KeyCode.R))
+        {
+          musicSource.clip = musicClipTwo;
+          musicSource.Play();
+          anim.SetInteger("State", 2);
+         }
+
+     if (Input.GetKeyUp(KeyCode.R))
+        {
+          musicSource.Stop();
+          anim.SetInteger("State", 0);
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.L))
+        {
+          musicSource.loop = true;
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+        {
+          musicSource.loop = false;
+        }
+
+     if (Input.GetKey("escape"))
+        {
+          Application.Quit();
+        }
+   }
+}
