3b3294bf5f5e1f0f24500726d2874b463648de24
wcombass99 <70278459+wcombass99@users.noreply.github.com>
2020-10-04 18:34:19 -0400

    First commit

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+void Update()
+{
+     if (Input.GetKeyDown(KeyCode.W))
+        {
+          musicSource.clip = musicClipOne;
+          musicSource.Play();
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+        {
+          musicSource.Stop();
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.R))
+        {
+          musicSource.clip = musicClipTwo;
+          musicSource.Play();
+         }
+
+     if (Input.GetKeyUp(KeyCode.R))
+        {
+          musicSource.Stop();
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.L))
+        {
+          musicSource.loop = true;
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+        {
+          musicSource.loop = false;
+        }
+   }
+}
